4338608603cdb96e17907fed53ed17743269f77c
naritayuuto <yuuto122narita@i.softbank.jp>
2022-10-26 16:46:09 +0900

    [Fix]PlayerControllerにUIManagerの関数読み込み処理追加。 [Add]NakaiEnemy（徘徊する敵）の簡単なscriptを追加。

--- Assets/Narita/Script/PlayerController.cs
+++ Assets/Narita/Script/PlayerController.cs
@@ -5,38 +5,41 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
-    /// <summary>動く速度</summary>
-    float _moveSpeed = 10f;
+    /// <summary>動く速度（子供）</summary>
+    float childMoveSpeed = 10f;
+    /// <summary>動く速度（大人）</summary>
+    float adultMoveSpeed = 15f;
     /// <summary>時計</summary>
-    float _timer = 0f;
+    float timer = 0f;
     /// <summary>枕を返すまでにかかる時間</summary>//atai
-    float _timerlimit = 0.5f;
-    Vector2 _moveVelocity;
+    float timerlimit = 0.5f;
+    Vector2 moveVelocity;
     /// <summary>止まる直前の速度方向</summary>
-    Vector2 _lastMoveVelocity;
+    Vector2 lastMoveVelocity;
     /// <summary>レベル</summary>
-    int _level = 1;
-    public VariableJoystick _joyStick;
-    Animator _anim;
-    Rigidbody2D _rb;
+    int level = 1;
+    public VariableJoystick joyStick;
+    Animator anim;
+    Rigidbody2D rb;
     /// <summary>枕を返す標的を保持する</summary>
-    Returnpillow _pillowEnemy;
-
+    Returnpillow pillowEnemy;
+    UIManager ui;
     /// <summary>枕返し圏内</summary>
-    bool _pillow = false;
+    bool pillow = false;
     /// <summary>大人か子供か</summary>
-    bool _adultState = false;
+    bool adultState = false;
 
     /// <summary>枕返し圏内</summary>
-    public bool Pillow { get => _pillow; set => _pillow = value; }
-
-    public int Level { get => _level; set => _level = value; }
-    public float Timerlimit { get => _timerlimit;}
+    public bool Pillow { get => pillow; set => pillow = value; }
+    public int Level { get => level; set => level = value; }
+    public float Timerlimit { get => timerlimit; set => timerlimit = value; }
+    public Returnpillow PillowEnemy { get => pillowEnemy; set => pillowEnemy = value; }
 
     void Start()
     {
-        _anim = GetComponent<Animator>();
-        _rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        ui = FindObjectOfType<UIManager>();
     }
 
     // Update is called once per frame
@@ -44,38 +47,46 @@ public class PlayerController : MonoBehaviour
     {
         //float h = Input.GetAxisRaw("Horizontal");
         //float v = Input.GetAxisRaw("Vertical");
-        float h = _joyStick.Horizontal;
-        float v = _joyStick.Vertical;
+        float h = joyStick.Horizontal;
+        float v = joyStick.Vertical;
 
-        _moveVelocity = new Vector2(h, v).normalized * _moveSpeed;
-        _rb.velocity = _moveVelocity;
-
-        _lastMoveVelocity = _moveVelocity;
-
-        if(Input.GetButton("Fire1"))//右長押し
+        if (!adultState)
         {
-            if(_pillowEnemy)//枕返し圏内にいたら
+            ChildMode(h, v);
+        }
+        else
+        {
+            AdultMode(h, v);
+        }
+
+        rb.velocity = moveVelocity;
+        lastMoveVelocity = moveVelocity;
+
+        if (Input.GetButton("Jump"))//右長押し
+        {
+            if (pillowEnemy)//枕返し圏内にいたら
             {
-                _timer += Time.deltaTime;
+                timer += Time.deltaTime;
+                //ui.ChargeSlider(timer);
             }
         }
     }
 
     void LateUpdate()
     {
-        if(_anim)
+        if (anim)
         {
             {//動いている間
-                _anim.SetFloat("floatの名前", _moveVelocity.x);
-                _anim.SetFloat("floatの名前", _moveVelocity.y);
+                anim.SetFloat("floatの名前", moveVelocity.x);
+                anim.SetFloat("floatの名前", moveVelocity.y);
                 //上の場合、＋Y
                 //下の場合、ーY
                 //右上、右下、右の場合、＋X
                 //左上、左下、左の場合、ーX
             }
             {//止まっている間
-                _anim.SetFloat("", _lastMoveVelocity.x);
-                _anim.SetFloat("", _lastMoveVelocity.y);
+                anim.SetFloat("", lastMoveVelocity.x);
+                anim.SetFloat("", lastMoveVelocity.y);
                 //上の条件に加えて、プレイヤーが動いていないこと。
             }
         }
@@ -83,22 +94,24 @@ public class PlayerController : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)//寝ている敵の情報を取る
     {
-        if(collision.gameObject.CompareTag("Enemy名"))
+        if (collision.gameObject.CompareTag("ReturnPillow"))
         {
-            _pillowEnemy = collision.GetComponent<Returnpillow>();
-
+            pillowEnemy = collision.GetComponent<Returnpillow>();
         }
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
-        _pillowEnemy = null;
+        pillowEnemy = null;
+        timer = 0;
     }
-    //void ReturnPillowCount()
-    //{
-    //    timer += Time.deltaTime;
-    //    if(timerlimit <= timer)
-    //    {
-           
-    //    }
-    //}
+
+    private void ChildMode(float h, float v)
+    {
+        moveVelocity = new Vector2(h, v).normalized * childMoveSpeed;
+    }
+
+    private void AdultMode(float h, float v)
+    {
+        moveVelocity = new Vector2(h, v).normalized * adultMoveSpeed;
+    }
 }
--- Assets/Narita/Script/Returnpillow.cs
+++ Assets/Narita/Script/Returnpillow.cs
@@ -3,45 +3,54 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Returnpillow: MonoBehaviour
+public class Returnpillow : MonoBehaviour
 {
-    //[SerializeField]
-    //Slider _slider = null;
     /// <summary>枕返しを行ったかどうか</summary>
-    bool _returnPillow;
-    /// <summary>起きる時間（基準）</summary>
-    float _getupTime;
+    bool returnPillow;
+    //Image image = null;
+    SpriteRenderer image = null;
+    /// <summary>起きる時間</summary>
+    float getupTime = 0f;
     /// <summary>playerが大人か子供かで変化する</summary>
-    float _timeInPlayerStats;
+    float _timeInPlayerStats = 0f;
     /// <summary>赤ん坊がいた場合使用する</summary>
-    float _timeInBaby;
+    float _timeInBaby = 0f;
     /// <summary>時計</summary>
-    float _timer;
-    Animator _anim = null;
-    Image _image = null;
-    public bool ReturnPillow { get => _returnPillow; set => _returnPillow = value; }
+    float timer;
+    Animator anim = null;
+    public bool ReturnPillow { get => returnPillow; set => returnPillow = value; }
     // Start is called before the first frame update
     void Start()
     {
-        _returnPillow = false;
-        _anim = GetComponent<Animator>();
-        _image = GetComponent<Image>();
+
+        returnPillow = false;
+        anim = GetComponent<Animator>();
+        //image = GetComponent<Image>();
+        image = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(_returnPillow)
+        if (returnPillow)
         {
             //↓当たり判定は後で変更予定
             GetComponent<BoxCollider2D>().enabled = false;
             //↓変える色は後で変更予定
-            _image.color = Color.black;
+            if (!image)
+            {
+                Debug.LogError("imageがありません");
+            }
+            else
+            {
+                image.color = Color.black;
+            }
         }
     }
     private void LateUpdate()
     {
-        _anim.SetBool("boolの名前", _returnPillow);
+        if (anim)
+            anim.SetBool("boolの名前", returnPillow);
     }
 
     private void OnTriggerStay2D(Collider2D collision)//プレイヤーが当たり判定の中にとどまったら
@@ -49,18 +58,23 @@ public class Returnpillow: MonoBehaviour
         if (collision.gameObject.CompareTag("Player"))
         {
             collision.GetComponent<PlayerController>().Pillow = true;
-            _timer += Time.deltaTime;
+            timer += Time.deltaTime;
             //数値の幅は後で変更予定
-            _getupTime = Random.Range(2, 5);
-            if (_getupTime <= _timer && _returnPillow)//制限時間を超えた　＋　枕を返されていなかったら
+
+            if (getupTime <= timer && returnPillow)//制限時間を超えた　＋　枕を返されていなかったら
             {
-                
+
             }
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        _timer -= _timer;
+        timer -= timer;
+    }
+
+    private void GetUpTime()
+    {
+
     }
 }

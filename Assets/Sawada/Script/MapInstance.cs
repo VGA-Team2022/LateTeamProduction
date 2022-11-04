7950c272e39f7fcbf1ff8b362aacebf67596d486
SR21C <rysawad@gmail.com>
2022-11-04 17:20:42 +0900

    [update]マップ生成に関するスクリプトを更新

--- Assets/Sawada/Script/MapInstance.cs
+++ Assets/Sawada/Script/MapInstance.cs
@@ -4,11 +4,26 @@ using UnityEngine;
 
 public class MapInstance : MonoBehaviour
 {
-    int _stageLevel = 0;
+    MapElemantEntity _entity = null;
+    Transform[] _instansTransform = null;
+
+    public MapElemantEntity Entity => _entity;
 
     void GenerateMap()
     {
-        
+        SpawnPosState[] insPos = null;
+        int[] houseValues = new int[] { _entity.HouseValue, _entity.HouseValueOnSolt, _entity.HouseValueInBaby, _entity.HouseValueInArrow };
+        int houseValueSum = 0;
+
+        _instansTransform = GetComponentsInChildren<Transform>();
+        foreach(int v in houseValues)
+        {
+            houseValueSum += v;
+        }
+        for (int i = 0; i < houseValueSum; i++)
+        {
+            insPos[i] = new SpawnPosState(_instansTransform[i]);
+        }
     }
 }
 
@@ -16,16 +31,15 @@ public struct SpawnPosState
 {
     public Transform SpawnPos;
     public SpawnState State;
+    public SpawnPosState(Transform spawnPos)
+    {
+        SpawnPos = spawnPos;
+        State = SpawnState.none;
+    }
     public enum SpawnState
     {
         none = 0,
         used = 1
     }
 }
-public enum HouseType
-{
-    None = 0,
-    Baby = 1,
-    Solt = 2,
-    DemonArrow = 3
-}
+

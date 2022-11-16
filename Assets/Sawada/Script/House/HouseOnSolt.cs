dedb5201e539dbfa8cbfd23f68cbba5df7f48d5e
SR21C <rysawad@gmail.com>
2022-11-16 15:30:25 +0900

    [update] 盛り塩の家のクラス内情報を更新

--- Assets/Sawada/Script/House/HouseOnSolt.cs
+++ Assets/Sawada/Script/House/HouseOnSolt.cs
@@ -1,21 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+
 
 public class HouseOnSolt : HouseBase
 {
-    Collider2D _collider = null;
+    [Tooltip("ドアのオブジェクトのの配列")]
+    GameObject[] _doorObjects = null;
+    Renderer[] _doorRenderers = null;
+    Collider2D[] _doorColliers = null;
 
-    public override void OnEnable()
+    public override void Init()
     {
-        
+        base.Init();
+        //取得
+        _doorObjects = GetComponentsInChildren<GameObject>().Where(x => x.tag == "Door").ToArray();
+        for(int i = 0; i < _doorObjects.Length; i++)
+        {
+            _doorRenderers[i] = _doorObjects[i].GetComponent<Renderer>();
+            _doorColliers[i] = _doorColliers[i].GetComponent<Collider2D>();
+        }
     }
-    public void OnDisable()
+
+    public override void HouseEntryMotion(PlayerController player)
     {
-        
-    }
-    public override void ObjectEntryMotion(PlayerController player)
-    {
-        base.ObjectEntryMotion(player);
+        base.HouseEntryMotion(player);
+        //プレイヤーの状況に応じてドアを開ける
+        //ToDo:プレイヤーの大人判定をプロパティで公開してもらう。
+        //foreach (Collider2D col in _doorColliers)
+        //{
+        //    col.enabled = player.AdaltState;
+        //}
+        //foreach (Renderer ren in _doorRenderers)
+        //{
+        //    ren.enabled = player.AdaltState;
+        //}
     }
 }

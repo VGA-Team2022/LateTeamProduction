430764c5b2d8b238a992d6a406ce7ea8fb04260e
SR21C <rysawad@gmail.com>
2022-11-18 23:30:38 +0900

    [update] 掛け軸の処理を更新

--- Assets/Sawada/Script/HangingScroll.cs
+++ Assets/Sawada/Script/HangingScroll.cs
@@ -5,15 +5,29 @@ using UnityEngine;
 [RequireComponent(typeof(CircleCollider2D))]
 public class HangingScroll : MonoBehaviour
 {
-    [SerializeField,Tooltip("掛け軸の画像。要素0が子供、要素1がおとな")]
+    [SerializeField, Tooltip("掛け軸の画像。要素0が子供、要素1がおとな")]
     Sprite[] _scrollImages = new Sprite[2];
+    [Tooltip("オブジェクトのRenderer")]
+    SpriteRenderer _scrollRenderer = null;
+
+    public void Init()
+    {
+        _scrollRenderer = GetComponent<SpriteRenderer>();
+    }
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.TryGetComponent<PlayerController>(out PlayerController player) && Input.GetButtonDown("Diside"))
         {
-            bool playerMode = !player.AdultState;
-            player.ModeChange(player);
-            //if
+            bool isAdultMode = !player.AdultState;
+            if (isAdultMode)
+            {
+                _scrollRenderer.sprite = _scrollImages[0];
+            }
+            else
+            {
+                _scrollRenderer.sprite = _scrollImages[1];
+            }
+            player.ModeChange(isAdultMode);
         }
     }
 }

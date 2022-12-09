afe8603f1e47cf7d86bc4f5288cdef85544badfd
ryotaro-ueda <charcoalueda0518@gmail.com>
2022-12-09 17:34:19 +0900

    [Fix] 変身用のカットインのサンプルとそのマテリアルを編集。[Add] カットインの関数をUIManagerに追加。GameManagerにゲーム中かどうかを示す bool 変数を追加。

--- Assets/Ueda/Script/GameManager.cs
+++ Assets/Ueda/Script/GameManager.cs
@@ -4,31 +4,39 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    /// <summary>残り時間</summary>
+    /// <summary>残り時間の初期値</summary>
     [SerializeField] float _timeLimit = 300;
-
-    float _clearTime = 0.0f;
-
-    [SerializeField] UIManager _uIManager = default;
     /// <summary>残りの敵（枕）の数</summary>
     [SerializeField] int _sleepingEnemy = 0;
     /// <summary>ゲームオーバー時に表示するUI </summary>
     [SerializeField] GameObject _gameOverUI = null;
-
+    [SerializeField] UIManager _uIManager = default;
     PlayerController _player = default;
     public int SleepingEnemy { get => _sleepingEnemy; set => _sleepingEnemy = value; }
     public PlayerController Player { get => _player; }
 
+    public bool _isGame = false; 
+
     MapInstance _mapInstance = default;
+
+
+    /// <summary>残り時間</summary>
+    float _time = 0;
     // Start is called before the first frame update
     void Start()
     {
         //_mapInstance = 
         //_sleepingEnemy = _mapInstance.Entity.SleeperValue;
-        _clearTime = _timeLimit;
         _player = FindObjectOfType<PlayerController>();
         _gameOverUI.SetActive(false);
+        GameStart();
     }
+
+    void GameStart()
+    {
+        _isGame = true;
+    }
+
 
     // Update is called once per frame
     void Update()
@@ -42,17 +50,19 @@ public class GameManager : MonoBehaviour
 
     private void Clear()
     {
-        _clearTime -= _timeLimit;
+        _uIManager.Clear(_timeLimit - _time);
         //Scene移動用関数を使用
+
     }
 
     private void Timer()
     {
-        _timeLimit -= Time.deltaTime;
-        _uIManager.TimerText(_timeLimit);
+        _time += Time.deltaTime;
+        _uIManager.TimerText(_timeLimit-_time);
     }
     public void GameOver()
     {
         _gameOverUI.SetActive(true);
+        _isGame = false;
     }
 }

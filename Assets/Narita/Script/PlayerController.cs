a7d1209e142ee23a2c1de80b2a65c2271edba7a4
naritayuuto <yuuto122narita@i.softbank.jp>
2022-12-10 02:04:35 +0900

    [Fix] 変更前に戻しました。 [update] 枕返しの敵の左右に移動する処理に変更を加えました。

--- Assets/Narita/Script/PlayerController.cs
+++ Assets/Narita/Script/PlayerController.cs
@@ -11,43 +11,78 @@ public class PlayerController : MonoBehaviour
     /// <summary>動く速度（大人）</summary>
     [SerializeField, Header("大人状態の動くスピード")]
     float _adultMoveSpeed = 15f;
+    [SerializeField, Header("誤差の許容範囲")]
+    float _toleranceDis = 0.5f;
+    /// <summary>時計</summary>
+    float _timer = 0f;
+    /// <summary>敵からどれだけ離れた距離から枕を返すかの間隔</summary>
+    float _returnPillowDisToEnemy = 0.5f;
+    /// <summary>プレイヤーと_returnPillowPosの距離</summary>
+    float _returnPillowDisToPlayer;
     /// <summary>移動速度計算結果</summary>
     Vector2 _moveVelocity;
     /// <summary>最後に移動していた方向</summary>
     Vector2 _lastMoveVelocity;
     /// <summary>敵の位置情報</summary>
     Vector2 _enemyPos = default;
+    /// <summary>プレイヤーが枕を返せる位置</summary>
+    Vector2 _returnPillowPos = default;
+    /// <summary>枕を返す標的のscriptを保持する</summary>
+    Returnpillow _pillowEnemy = null;
+    /// <summary>枕を返す標的のgameobjectを保持する</summary>
+    GameObject _pillowEnemyObject = null;
     /// <summary>大人か子供か</summary>
     [SerializeField, Header("プレイヤーが大人か子供か")]
     bool _adultState = false;
+    [SerializeField, Header("枕を返せる場所にいるかどうか,返しているときのみTrue")]
+    public bool _returnPillowInPos = false;
     [SerializeField,Header("枕の横に自動的に移動しているときにtrue")]
-    public bool _autoAnim = false;
+    bool _autoAnim = false;
     Rigidbody2D _rb;
-    GameManager _gm;
     UIManager _ui;
+    GameManager _gm;
     Animator _anim = null;
     /// <summary>プレイヤーの状態確認、外部参照用</summary>
     public bool AdultState { get => _adultState; }
     /// <summary>枕を返す標的のscript</summary>
-
-    //public Vector2 ReturnPillowPos { get => _returnPillowPos; }
+    public Returnpillow PillowEnemy { get => _pillowEnemy; set => _pillowEnemy = value; }
+    /// <summary>枕を返す標的のgameobject</summary>
+    public GameObject PillowEnemyObject { get => _pillowEnemyObject; set => _pillowEnemyObject = value; }
+    public Vector2 ReturnPillowPos { get => _returnPillowPos; }
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
-        _anim = GetComponent<Animator>();
-        _gm = FindObjectOfType<GameManager>();
         _ui = FindObjectOfType<UIManager>();
+        _anim = GetComponent<Animator>();
     }
     // Update is called once per frame
     void Update()
     {
         float _joyX = _joyStick.Horizontal;
         float _joyY = _joyStick.Vertical;
-        //Debug.Log(_rb.velocity);
+        Debug.Log(_rb.velocity);
         ModeCheck(_joyX, _joyY);
         //↑ここで計算された_moveVelocityを代入
         _rb.velocity = _moveVelocity;
-        VelocitySave(_rb.velocity); 
+        VelocitySave(_rb.velocity);
+        if (Input.GetButton("Jump"))//スペース長押し
+        {
+            if (_pillowEnemy)//枕返し圏内にいたら
+            {
+                if (!_adultState)
+                    TranslatePlayerPos(_childMoveSpeed);
+                else
+                    TranslatePlayerPos(_adultMoveSpeed);
+            }
+        }
+        else
+        {
+            _returnPillowInPos = false;
+        }
+        if (Input.GetButtonDown("Jump"))//自動で動くために距離計算を行う,スペースキー一回
+        {
+            PlayerAndEnemyDis();
+        }
     }
     private void LateUpdate()
     {
@@ -58,7 +93,20 @@ public class PlayerController : MonoBehaviour
         _anim.SetFloat("LastVeloX", _lastMoveVelocity.x);
         _anim.SetFloat("LastVeloY", _lastMoveVelocity.y);
         _anim.SetBool("adultState", _adultState);
+        _anim.SetBool("returnPillowInPos", _returnPillowInPos);
         _anim.SetBool("autoMode", _autoAnim);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)//寝ている敵の情報を取る
+    {
+        if (collision.gameObject.CompareTag("ReturnPillow"))
+        {
+            _pillowEnemyObject = collision.gameObject;
+            _pillowEnemy = _pillowEnemyObject.GetComponent<Returnpillow>();
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        InformationReset();
     }
 
     private void ModeCheck(float h, float v)
@@ -83,10 +131,66 @@ public class PlayerController : MonoBehaviour
     }
     public void ModeChange(bool change)//大人化、子供化するときに呼び出す関数
     {
-        _ui.CutIn(_adultState);
         _adultState = change;
     }
+    public void InformationReset()//取得したデータ全消し、スライダーの初期化
+    {
+        _pillowEnemyObject = null;
+        _pillowEnemy = null;
+        _timer = 0;
+        _ui.ChargeSlider(_timer);
+    }
 
+    private void PlayerAndEnemyDis()//距離計算
+    {
+        if(_pillowEnemyObject)
+            _returnPillowPos = Vector2.Distance(transform.position, _pillowEnemy.ReturnPillouPos[1].transform.position)
+            >= Vector2.Distance(transform.position, _pillowEnemy.ReturnPillouPos[2].transform.position) ?
+            _pillowEnemy.ReturnPillouPos[2].transform.position : _pillowEnemy.ReturnPillouPos[1].transform.position;
+        else
+            Debug.Log("敵の位置情報を取得出来ていません");
+    }
+    private void TranslatePlayerPos(float speed)
+    {
+        if (_pillowEnemyObject)//Enemy情報を持っていたら
+        {
+            _autoAnim = true;
+            _returnPillowDisToPlayer = Vector2.Distance(transform.position, _returnPillowPos);
+            if (_returnPillowDisToPlayer > _toleranceDis)//誤差範囲
+            {
+                if (Mathf.Abs(transform.position.x - _returnPillowPos.x) > _toleranceDis)
+                {
+                    if (transform.position.x > _returnPillowPos.x)
+                    {
+                        transform.Translate(Vector2.left * Time.deltaTime * speed);
+                        _rb.velocity = Vector2.left;
+                    }
+                    else if (transform.position.x < _returnPillowPos.x)
+                    {
+                        transform.Translate(Vector2.right * Time.deltaTime * speed);
+                        _rb.velocity = Vector2.right;
+                    }
+                }
+                else
+                {
+                    if (transform.position.y > _returnPillowPos.y)
+                    {
+                        transform.Translate(Vector2.down * Time.deltaTime * speed);
+                        _rb.velocity = Vector2.down;
+                    }
+                    else if (transform.position.y < _returnPillowPos.y)
+                    {
+                        transform.Translate(Vector2.up * Time.deltaTime * speed);
+                        _rb.velocity = Vector2.up;
+                    }
+                }
+            }
+            else
+                _returnPillowInPos = true;
+                _timer += Time.deltaTime;
+                _ui.ChargeSlider(_timer);
+        }
+    }
     /// <summary>見つかった場合呼ぶ,アニメーションイベント専用関数</summary>
     public void PlayerFind()
     {

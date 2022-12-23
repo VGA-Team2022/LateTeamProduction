aec6a277da48a227566e018b1d1bc821e0c7bf54
naritayuuto <yuuto122narita@i.softbank.jp>
2022-12-23 19:41:12 +0900

    [Fix] 寝ている人の頭の上に出すGameobject自体にアニメーションは必要ないと判断し、GameobjectのSetActiveの切り替えで表示。 寝ている人が起きたときには、そのまま怒るアニメーションに移動するので、起きたときのアニメーションと、怒るアニメーションの中に「頭の上に出すGameobject」のSpriteを切り替えるアニメーションを追加しました。 動作確認済み。

--- Assets/Narita/Script/Returnpillow.cs
+++ Assets/Narita/Script/Returnpillow.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Returnpillow : MonoBehaviour,IRevers
+public class Returnpillow : MonoBehaviour, IRevers
 {
     [SerializeField, Header("枕を返す時のプレイヤーの位置"), Tooltip("枕を返す時のプレイヤーの位置情報")]
     Transform _returnPillouPosLeft;
@@ -11,10 +11,14 @@ public class Returnpillow : MonoBehaviour,IRevers
     Transform _returnPillouPosRight;
     [SerializeField, Header("枕を返されたかどうか"), Tooltip("枕を返されたかどうか")]
     bool _returnPillow = false;
-    [SerializeField,Header("起きたかどうか"),Tooltip("起きたかどうか")]
+    [SerializeField, Header("起きたかどうか"), Tooltip("起きたかどうか")]
     bool _getUp = false;
+    [SerializeField, Header("Playerが侵入している場合True")]
+    bool _playerIntrusion = false;
     [SerializeField, Header("起きる時間（基準）"), Tooltip("起きる時間（関数内で値を決める）")]
     float _getupTime = 5f;
+    [Tooltip("reactionを表示する秒数のborder、_getupCountTimerの半分")]
+    float _reactionTime = 2.5f;
     //
     [SerializeField, Header("プレイヤーの状態で変化する時間"), Tooltip("プレイヤーが大人の時値を変化させる")]
     float _timeInPlayerStats = 0f;
@@ -24,54 +28,69 @@ public class Returnpillow : MonoBehaviour,IRevers
     //,「//」で囲っている変数はこのscriptが持つべきなのかわからないが、話に上がっているのにどこにも存在しないため定義している。
     [Tooltip("起きるまでの時間をカウントするタイマー")]
     float _getupCountTimer;
-    [SerializeField,Tooltip("reactionのanimator"),Header("リアクションするときに使うアニメーター")]
-    Animator _reactionAnim = null;
+    [SerializeField, Tooltip("reactionする場所に設置してあるGameobject")]
+    GameObject _reactionObject = null; 
+    [SerializeField, Tooltip("もやもやの画像")]
+    Sprite _reaction = null;
+    [Tooltip("自身のanimator")]
     Animator _returnPillowAnim = null;
     [Tooltip("当たり判定の配列")]
     Collider2D[] collider = null;
     SleepingEnemyAnimControle _sleepHumanController = null;
-    [SerializeField,Tooltip("playerを見つけたときに使用")]
+    [SerializeField, Tooltip("playerを見つけたときに使用")]
     SoundManager _sound = null;
     [Tooltip("枕を返されたかどうか、外部参照用")]
     public bool ReturnPillow { get => _returnPillow; set => _returnPillow = value; }
     [Tooltip("枕を返す時のプレイヤーの位置情報、外部参照用")]
-    public Transform ReturnPillouPosLeft { get => _returnPillouPosLeft;}
+    public Transform ReturnPillouPosLeft { get => _returnPillouPosLeft; }
     public Transform ReturnPillouPosRight { get => _returnPillouPosRight; }
     // Start is called before the first frame update
     void Start()
     {
         _returnPillow = false;
+        _reactionObject.SetActive(false);
         _sleepHumanController = GetComponent<SleepingEnemyAnimControle>();
         _returnPillowAnim = GetComponent<Animator>();
         collider = GetComponents<Collider2D>();
     }
 
+
+    void Update()
+    {
+        if(_playerIntrusion)
+        {
+            _getupCountTimer += Time.deltaTime;
+            //カウントがreactionする時間のborderを超えていて、起きる時間より小さかったら
+            if (_getupCountTimer > _reactionTime && _getupCountTimer < _getupTime)
+            {
+                _reactionObject.SetActive(true);
+            }
+            if (_getupTime < _getupCountTimer)//制限時間を超えた + 枕を返されていなかったら 
+            {
+                _sound.Discoverd();
+                _sleepHumanController.Awaken();
+                _getUp = true;
+                _sleepHumanController.Discover();
+                //何秒か遅らせてから
+                IsGame.GameManager.Instance.GameOver();
+            }
+        }
+    }
     // Update is called once per frame
 
     private void OnTriggerStay2D(Collider2D collision)//プレイヤーが当たり判定の中にとどまったら
     {
         if (collision.TryGetComponent<PlayerController>(out PlayerController player))
         {
-            _getupCountTimer += Time.deltaTime;
-            if (_getupTime <= _getupCountTimer && _returnPillow)//制限時間を超えた + 枕を返されていなかったら 
-            {
-                _sleepHumanController.Awaken();
-                _returnPillowAnim.SetFloat("getUpTime", _getupCountTimer);
-                if (collision.TryGetComponent<PlayerController>(out PlayerController p))//プレイヤーがまだ範囲内にいたら
-                {
-                    _getUp = true;
-                    _returnPillowAnim.SetBool("getUp", _getUp);
-                    _sound.Discoverd();
-                    _sleepHumanController.Discover();
-                    player.PlayerFind();
-                }
-            }
+            _playerIntrusion = true;
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         _getupCountTimer = 0;
+        _playerIntrusion = false;
+        _reactionObject.SetActive(false);
     }
     /// <summary>起きる時間を決める、プレイヤーの状態が変化したときに呼ぶ関数</summary>
     /// <param name="time"></param>

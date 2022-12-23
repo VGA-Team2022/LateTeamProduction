f706efb69cfa97852d305670bbe080fc662e2679
naritayuuto <yuuto122narita@i.softbank.jp>
2022-12-23 19:34:29 +0900

    [Fix] 寝ている敵のreaction(鼻提灯や、頭の上に表示するもの)のアニメーションを修正。 [Delete] 一部必要のない記述削除。

--- Assets/Tozawa/TozawaScript/SleepingEnemyAnimControle.cs
+++ Assets/Tozawa/TozawaScript/SleepingEnemyAnimControle.cs
@@ -18,14 +18,17 @@ public class SleepingEnemyAnimControle : MonoBehaviour
         _bodyAnim.SetTrigger("IsAwake");
         _reactionAnim.SetTrigger("IsAwake");
     }
+    ///// <summary>
+    ///// Ä“xQ‚é‚ÉŒÄ‚ñ‚Å‚­‚¾‚³‚¢
+    ///// </summary>
+    //public void Sleeping()
+    //{
+    //    _bodyAnim.SetTrigger("IsSleep");
+    //    _reactionAnim.SetTrigger("IsSleep");
+    //}
     /// <summary>
-    /// Ä“xQ‚é‚ÉŒÄ‚ñ‚Å‚­‚¾‚³‚¢
+    /// ‹N‚«‚½‚Æ‚«‚ÉŒÄ‚Ô
     /// </summary>
-    public void Sleeping()
-    {
-        _bodyAnim.SetTrigger("IsSleep");
-        _reactionAnim.SetTrigger("IsSleep");
-    }
     public void Discover()
     {
         _bodyAnim.SetTrigger("IsDiscover");

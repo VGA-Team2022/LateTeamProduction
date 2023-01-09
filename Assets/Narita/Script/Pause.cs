23abe5cfe741cf9428305506871e2812cbddb890
naritayuuto <yuuto122narita@i.softbank.jp>
2023-01-10 08:10:18 +0900

    [Fix] ステージ追加するたびにセットするのが面倒と感じたため、Listに変更し、タグで識別し取得するように変更。

--- Assets/Narita/Script/Pause.cs
+++ Assets/Narita/Script/Pause.cs
@@ -5,26 +5,36 @@ using UnityEngine;
 public class Pause : MonoBehaviour
 {
     [SerializeField]
-    GameObject[] _moveObject;
+    List<GameObject> _objList;
     [SerializeField, Header("pauseƒ{ƒ^ƒ“‚ª‰Ÿ‚³‚ê‚½‚çTrue")]
     bool _isPause = false;
 
     /// <summary>True‚ÌAŠÔ‚Ì‰ÁZ‚ğ~‚ß‚é</summary>
     public bool IsPause { get => _isPause; }
-    
-  
+
+    private void Start()
+    {
+        _objList = new List<GameObject>();
+        _objList.Add(GameObject.FindGameObjectWithTag("Player"));
+        GameObject[] enemys = GameObject.FindGameObjectsWithTag("NakaiEnemy");
+        foreach (var enemy in enemys)
+        {
+            _objList.Add(enemy);
+        }
+    }
+
     public void PauseAction()
     {
         if (!_isPause)
         {
-            foreach (var obj in _moveObject)
+            foreach (var obj in _objList)
             {
                 obj.SetActive(false);
             }
         }
         else
         {
-            foreach (var obj in _moveObject)
+            foreach (var obj in _objList)
             {
                 obj.SetActive(true);
             }
